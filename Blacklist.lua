6ed7b056fb96c51ddcfb646fa217b0538e6d1e6a
ChinaQY <743282937@qq.com>
2025-04-18 21:23:32 +0800

    更新 Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,13 +1,13 @@
 return {
-        [1] = {"AQ14ABS", true, "LOL"}
-        [2] = {"edc12363", true, "LOL"}
-        [3] = {"onygfvjop", true, "LOL"}
-        [4] = {"ah_kdj3", true, "LOL"}
-        [5] = {"luoyang100616", true, "LOL"}
-        [6] = {"hdjdje675", true, "LOL"}
-        [7] = {"hvck114", true, "LOL"}
-        [8] = {"CC_hdcu", true, "LOL"}
-        [9] = {"rzc1238", true, "LOL"}
-        [10] = {"rzc12378", true, "LOL"}
+        [1] = {"AQ14ABS", true, "LOL"},
+        [2] = {"edc12363", true, "LOL"},
+        [3] = {"onygfvjop", true, "LOL"},
+        [4] = {"ah_kdj3", true, "LOL"},
+        [5] = {"luoyang100616", true, "LOL"},
+        [6] = {"hdjdje675", true, "LOL"},
+        [7] = {"hvck114", true, "LOL"},
+        [8] = {"CC_hdcu", true, "LOL"},
+        [9] = {"rzc1238", true, "LOL"},
+        [10] = {"rzc12378", true, "LOL"},
         [11] = {"rzc12398", true, "LOL"}
 }
