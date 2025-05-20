f6e947d7230ac4e8bab8eac9d0223c2cc7bf38e9
ChinaQY <743282937@qq.com>
2025-05-20 21:04:05 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,14 +1,15 @@
 return {
         [1] = {"jmeoo114514", false, nil},
-        [2] = {"Hjomnjbvhgc", false, nil},
-        [3] = {"AQ14ABS", true, "LOL"},
-        [4] = {"edc12363", true, "LOL"},
-        [5] = {"onygfvjop", true, "LOL"},
-        [6] = {"ah_kdj3", true, "LOL"},
-        [7] = {"luoyang100616", true, "LOL"},
-        [8] = {"hdjdje675", true, "LOL"},
-        [9] = {"qazwsxuruu", true, "瞎骂"},
-        [10] = {"ADCZ4xx", true, "LOL"},
-        [11] = {"TM5418888", true, "牢营长"},
-        [12] = {"codm656558", true, "LOL"}
+        [2] = {"Brina1090", false, nil},
+        [3] = {"Hjomnjbvhgc", false, nil},
+        [4] = {"AQ14ABS", true, "LOL"},
+        [5] = {"edc12363", true, "LOL"},
+        [6] = {"onygfvjop", true, "LOL"},
+        [7] = {"ah_kdj3", true, "LOL"},
+        [8] = {"luoyang100616", true, "LOL"},
+        [9] = {"hdjdje675", true, "LOL"},
+        [10] = {"qazwsxuruu", true, "瞎骂"},
+        [11] = {"ADCZ4xx", true, "LOL"},
+        [12] = {"TM5418888", true, "牢营长"},
+        [13] = {"codm656558", true, "LOL"}
 }
