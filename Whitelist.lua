2b2902d9d3bc9da5c5f1c8a5ae9b9bf83e3552b2
ChinaQY <743282937@qq.com>
2025-05-20 17:32:32 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,12 +1,13 @@
 return {
-        [1] = {"AQ14ABS", true, "LOL"},
-        [2] = {"edc12363", true, "LOL"},
-        [3] = {"onygfvjop", true, "LOL"},
-        [4] = {"ah_kdj3", true, "LOL"},
-        [5] = {"luoyang100616", true, "LOL"},
-        [6] = {"hdjdje675", true, "LOL"},
-        [7] = {"qazwsxuruu", true, "瞎骂"},
-        [8] = {"ADCZ4xx", true, "LOL"},
-        [9] = {"TM5418888", true, "牢营长"},
-        [10] = {"codm656558", true, "LOL"}
+        [1] = {"jmeoo114514", false, nil}
+        [2] = {"AQ14ABS", true, "LOL"},
+        [3] = {"edc12363", true, "LOL"},
+        [4] = {"onygfvjop", true, "LOL"},
+        [5] = {"ah_kdj3", true, "LOL"},
+        [6] = {"luoyang100616", true, "LOL"},
+        [7] = {"hdjdje675", true, "LOL"},
+        [8] = {"qazwsxuruu", true, "瞎骂"},
+        [9] = {"ADCZ4xx", true, "LOL"},
+        [10] = {"TM5418888", true, "牢营长"},
+        [11] = {"codm656558", true, "LOL"}
 }
