56bc208edf7e6c111d3f5d71af3b1583bb2ff65e
ChinaQY <743282937@qq.com>
2025-05-21 21:04:13 +0800

    更新 Test.lua

--- Test.lua
+++ Test.lua
@@ -1,6 +1,4 @@
-{
-    "Test" : {
-        "Version" : "1.0",
-        "Blacklist" : true
-    }
+return {
+        [1] = {"jmeoo114514", false},
+        [2] = {"b549299c-8c7d-4eb3-92df-bdcd2ca110ac", false}
 }
