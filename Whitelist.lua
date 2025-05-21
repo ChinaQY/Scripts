2ec92ef8324e01d80ad84ed0f111a76e4c68a21b
ChinaQY <743282937@qq.com>
2025-05-21 17:48:39 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,15 +1,1 @@
-return {
-        [1] = {"jmeoo114514", false, nil},
-        [2] = {"Brina1090", false, nil},
-        [3] = {"Hjomnjbvhgc", false, nil},
-        [4] = {"AQ14ABS", true, "LOL"},
-        [5] = {"edc12363", true, "LOL"},
-        [6] = {"onygfvjop", true, "LOL"},
-        [7] = {"ah_kdj3", true, "LOL"},
-        [8] = {"luoyang100616", true, "LOL"},
-        [9] = {"hdjdje675", true, "LOL"},
-        [10] = {"qazwsxuruu", true, "瞎骂"},
-        [11] = {"ADCZ4xx", true, "LOL"},
-        [12] = {"TM5418888", true, "牢营长"},
-        [13] = {"codm656558", true, "LOL"}
-}
+?
