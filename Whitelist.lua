bd824b37a9b8ccb272cdd689aef6a359d23c46cd
ChinaQY <743282937@qq.com>
2025-05-21 19:01:59 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,3 +1,4 @@
 return {
-        [1] = {"ea574d9f-9cf9-4259-a35f-5600f392726f", "jmeoo114514", false, nil}
+        [1] = {"ea574d9f-9cf9-4259-a35f-5600f392726f", "", false, nil},
+        [1] = {"jmeoo114514", true, "理由"}
 }
