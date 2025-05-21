6d38cc987888df4fa2786abc75c54c49784c70e9
ChinaQY <743282937@qq.com>
2025-05-22 02:12:10 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,5 +1,5 @@
 return {
-        [1] = {"28d7ee047153da3d8e28b92fb76bb4420f55588542606d4284b00b469dc12189ddaadfe58fc76613c982c17aa74d47e2", false},
+        [1] = {"?", false},
         [2] = {"b549299c-8c7d-4eb3-92df-bdcd2ca110ac", false},
         [3] = {"02e26298-266a-40d8-b559-c940e3806c16", false}
 }
