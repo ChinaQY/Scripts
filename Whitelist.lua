777134ec01d1e0bef9a30d33b48d0c0a4ba51a16
ChinaQY <743282937@qq.com>
2025-05-21 20:41:43 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,3 +1,4 @@
 return {
-        [1] = {"ea574d9f-9cf9-4259-a35f-5600f392726f", true, "lol"}
+        [1] = {"ea574d9f-9cf9-4259-a35f-5600f392726f", false},
+        [2] = {"b549299c-8c7d-4eb3-92df-bdcd2ca110ac", false}
 }
