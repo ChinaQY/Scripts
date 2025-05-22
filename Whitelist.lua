ea916d90b43d083c64c081387c042ed1aff2180b
ChinaQY <743282937@qq.com>
2025-05-22 15:43:59 +0800

    更新 Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,5 +1,5 @@
 return {
         [1] = {"", false},
-        [2] = {"b549299c-8c7d-4eb3-92df-bdcd2ca110ac", false},
+        [2] = {"f18cd662040d07fcc8efa75fc28eeab99941daa26819d9f81cccd23fc759ce058d4aea8cbf997d790a8b997e4a8d5b5a", false},
         [3] = {"02e26298-266a-40d8-b559-c940e3806c16", false}
 }
